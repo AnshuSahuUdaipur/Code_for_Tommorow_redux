2d6b1d7805e8d9f2530bbb343af5612bdcb44a97
Anshu <anshu9691610179@gmail.com>
2024-08-29 17:07:43 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
  <Provider store={store}>
     <BrowserRouter>
 
-    <App />
+    <App />git commit -m "first commit"
 
     </BrowserRouter>
     
